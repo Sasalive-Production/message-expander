0a1c79a9f8c3d031d410f9799d802532f2bcdbb0
Kigou-No1 <53385458+Kigou-No1@users.noreply.github.com>
2024-11-17 04:09:31 +0900

    impl: process message content

--- events/messageCreate.go
+++ events/messageCreate.go
@@ -5,43 +5,93 @@ import (
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
+	"log/slog"
 )
 
-var url_expression string = `https://(?:ptb\.|canary\.)?discord\.com/channels/(\d{17,19})/(\d{17,19})/(\d{17,19})`
-var id_expression string = `(\d{17,19})/(\d{17,19})/(\d{17,19})`
+var discord_url_expression string = `https://(?:ptb\.|canary\.)?discord\.com/channels/(\d{17,19})/(\d{17,19})/(\d{17,19})`
+var url_expression string = `https?://[\w/:%#\$&\?\(\)~\.=\+\-]+`
+var id_expression = `(\d{17,19})/(\d{17,19})/(\d{17,19})`
 
-func getIDs(url string) []string {
-	url_regex, _ := regexp.Compile(url_expression)
-	channels_regex, _ := regexp.Compile(id_expression)
-	ids := strings.Split(channels_regex.FindAllString(url_regex.FindString(url), -1)[0], "/")
-	return ids
+var discord_url_regex = regexp.MustCompile(discord_url_expression)
+var url_regex = regexp.MustCompile(url_expression)
+var ids_regex = regexp.MustCompile(id_expression)
+
+type messageInfo struct {
+	guild string;
+	channel string;
+	message string;
+}
+
+type discordInfo struct {
+	guild *discordgo.Guild;
+	channel *discordgo.Channel;
+	message *discordgo.Message;
+}
+
+func getIDs(url string) ( messageInfo ) {
+	ids := strings.Split(ids_regex.FindAllString(url_regex.FindString(url), -1)[0], "/")
+	return messageInfo{guild: ids[0], channel: ids[1], message: ids[2]}
+}
+
+func getDiscordInformationFromURL(s *discordgo.Session, url string) ( discordInfo, error ) {
+	msg_info := getIDs(url);
+	guild, err := s.State.Guild(msg_info.guild)
+	if err != nil {
+		slog.Error("An error occured when retrieving guild", err.Error())
+		return discordInfo{}, err
+	}
+	channel, err := s.Channel(msg_info.channel)
+	if err != nil {
+		slog.Error("An error occured when retrieving channel", err.Error())
+		return discordInfo{}, err
+	}
+	message, err := s.ChannelMessage(msg_info.channel, msg_info.message)
+	if err != nil {
+		slog.Error("An error occured when retrieving message", err.Error())
+		return discordInfo{}, err
+	}
+
+	return discordInfo{guild: guild, channel: channel, message: message}, nil
 }
 
 func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	url_regex, _ := regexp.Compile(url_expression)
-
-	if m.Author.ID == s.State.User.ID {
-		return
+	if m.Author.ID == s.State.User.ID || m.Author.Bot {
+		slog.Info("Ignoring bot message")
 	}
 
-	if url_regex.MatchString(m.Content) {
-		urls := url_regex.FindAllString(m.Content, -1);
+	if discord_url_regex.MatchString(m.Content) {
+		urls := discord_url_regex.FindAllString(m.Content, -1);
 		embeds := []*discordgo.MessageEmbed{}
 		for _, url := range urls {
-			ids := getIDs(url);
-			message, err := s.State.Message(ids[1], ids[2])
+		
+			discord_info, err := getDiscordInformationFromURL(s, url)
 			if err != nil {
 				return
 			}
-			
-			guild, err := s.State.Guild(message.GuildID)
-			if err != nil {
-				return
+
+			message := discord_info.message
+			guild := discord_info.guild
+
+			if url_regex.MatchString(message.Content) {
+				for _, url := range url_regex.FindAllString(message.Content, -1) {
+					if discord_url_regex.MatchString(url) {
+						discord_info, err := getDiscordInformationFromURL(s, url)
+						if err != nil {
+							return
+						}
+						message.Content = strings.ReplaceAll(message.Content, url, "[" + discord_info.message.Content[8:28] + "](" + url + ")")
+
+					} else {
+						message.Content = strings.ReplaceAll(message.Content, url, "[" + url[8:28] + "](" + url + ")")
+					}
+				}
 			}
 
 			if len(message.Content) > 2048 {
 				message.Content = message.Content[:2045] + "..."
+				slog.Info("The message content is too long, it has been truncated")
 			}
+
 			timestamp, _ := discordgo.SnowflakeTimestamp(message.ID);
 
 			embed := &discordgo.MessageEmbed{
@@ -57,7 +107,7 @@ func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 				},
 			}
 
-			if message.Attachments != nil {
+			if len(message.Attachments) != 0 {
 				embed.Image = &discordgo.MessageEmbedImage{
 					URL: message.Attachments[0].URL,
 				}
@@ -65,7 +115,10 @@ func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 			embeds = append(embeds, embed)
 		}
-		s.ChannelMessageSendEmbedsReply(m.ChannelID, embeds, m.Reference());
+		_,e := s.ChannelMessageSendEmbedsReply(m.ChannelID, embeds, m.Reference());
+		if e != nil {
+			println(e.Error())
+		}
 	}
 }
 
--- events/messageCreate_test.go
+++ events/messageCreate_test.go
@@ -4,9 +4,9 @@ import ( "testing" )
 
 func  TestGetIDs(t *testing.T) {
 	url := "https://discord.com/channels/123456789012345678/123456789012345678/123456789012345678"
-	expected := []string{"123456789012345678", "123456789012345678", "123456789012345678"}
+	expected := messageInfo{guild: "123456789012345678", channel: "123456789012345678", message: "123456789012345678"}
 	actual := getIDs(url);
-	if actual[0] != expected[0] || actual[1] != expected[1] || actual[2] != expected[2] {
+	if actual != expected {
 		t.Error("Expected", expected, "got", actual)
 	}
 }
