04b26606bb2c71908fce4b427360dc7b2d21a9c6
Kigou-No1 <53385458+Kigou-No1@users.noreply.github.com>
2024-11-17 04:09:43 +0900

    go.mod

--- main.go
+++ main.go
@@ -6,11 +6,22 @@ import (
 	"os/signal"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 	"message-expander/events"
 );
 
+func Env_load() {
+    err := godotenv.Load()
+    if err != nil {
+       println("Error loading .env file") 
+	   os.Exit(1);
+    }
+}
+
 func main() {
-	dg, err := discordgo.New("Bot "+ "token")
+	Env_load();
+
+	dg, err := discordgo.New("Bot "+ os.Getenv("DISCORD_TOKEN"))
 	if err != nil {
 		fmt.Println("error creating Discord session,", err)
 		return
@@ -28,6 +39,7 @@ func main() {
 	fmt.Println("Logged in as", dg.State.User.Username);
 	sc := make(chan os.Signal, 1);
 	signal.Notify(sc, os.Interrupt, os.Kill);
+	
 	<-sc;
 	dg.Close();
 }
